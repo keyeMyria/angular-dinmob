d5719f1db81e6eebd735a51250b4f4ca4b20eb7c
Ramiro Arechar <arechar@local.com>
2017-09-21 13:25:13 -0500

    get lotes

--- src/app/components/ventas/desarrollos/desarrollos.component.ts
+++ src/app/components/ventas/desarrollos/desarrollos.component.ts
@@ -16,20 +16,22 @@ import { ObrasService } from "app/services/obras.service";
 
 })
 export class DesarrollosComponent implements OnInit {
-  lotes: any[] = [];
+ 
   obras: any = [];
   obra: any = {
     datos: {}
   };
-  obras_selected: any = {};
+  obra_selected: any = {};
  
-  constructor(private obraSrv: ObrasService,) { }
+  constructor(
+    private obraSrv: ObrasService
+  ) { }
 
 
 
   ngOnInit() {
 
-    this.obraSrv.loadFullObra(58)
+    this.obraSrv.getLotes(58)
     .subscribe(response => {
       this.obra = response;
       console.log("obra", this.obra);
@@ -38,17 +40,9 @@ export class DesarrollosComponent implements OnInit {
     this.obraSrv.getObrasUsuario(18)
     .subscribe(response => {
       this.obras = response;
-    });
-  
+    }); 
 
-    this.lotes = [
-      { nombre: "Lote 1", manzana: "Manzana 1", precio: 1000000.00, estado: "Libre" },
-      { nombre: "Lote 2", manzana: "Manzana 1", precio: 1000000.00, estado: "Libre" },
-      { nombre: "Lote 3", manzana: "Manzana 1", precio: 1000000.00, estado: "Libre" },
-      { nombre: "Lote 4", manzana: "Manzana 1", precio: 1000000.00, estado: "Libre" },
-      { nombre: "Lote 5", manzana: "Manzana 1", precio: 1000000.00, estado: "Libre" },
-      { nombre: "Lote 6", manzana: "Manzana 1", precio: 1000000.00, estado: "Libre" }
-    ];
+    
   }
 
 }
