9b68d99e0b13779d75dadf382174811e672a32e5
Ramiro Arechar <ramiro@arechar>
2018-01-27 20:33:00 -0600

    style

--- src/app/components/ventas/editar-cliente/editar-cliente.component.ts
+++ src/app/components/ventas/editar-cliente/editar-cliente.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from "app/model/cliente";
-import { MatDialog, MatSnackBar } from "@angular/material";
+import { MatDialog, MatSnackBar, MatTabChangeEvent } from "@angular/material";
 import { AgregarDocumentoDialogoComponent } from "app/components/ventas/agregar-documento-dialogo/agregar-documento-dialogo.component";
 import { ActivatedRoute, Router, ParamMap } from "@angular/router";
 import "rxjs/add/operator/switchMap";
@@ -29,14 +29,14 @@ export class EditarClienteComponent implements OnInit {
   });
 
   public maskRFC = [/[A-Z0-9]/i, /[A-Z0-9]/i, /[A-Z0-9]/i, /[A-Z0-9]/i, '-', /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, '-', /[A-Z0-9]/i, /[A-Z0-9]/i, /[A-Z0-9]/i,]
-  public maskCURP = [/[A-Z1-9]/i, /[A-Z1-9]/i, /[A-Z1-9]/i, /[A-Z1-9]/i, '-', /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, '-', /[A-Z]/i, '-', /[A-Z]/i, /[A-Z]/i, '-', /[A-Z1-9]/i, /[A-Z1-9]/i, /[A-Z1-9]/i, '-', /[A-Z1-9]/i, /[A-Z]/i]
+  public maskCURP = [/[A-Z0-9]/i, /[A-Z0-9]/i, /[A-Z0-9]/i, /[A-Z0-9]/i, '-', /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, '-', /[A-Z]/i, '-', /[A-Z]/i, /[A-Z]/i, '-', /[A-Z0-9]/i, /[A-Z0-9]/i, /[A-Z0-9]/i, '-', /[A-Z0-9]/i, /[A-Z0-9]/i]
   public maskCP = [/\d/, /\d/, /\d/, /\d/, /\d/]
   public maskPhone = ['(', /\d/, /\d/, /\d/, ')', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]
   public maskRFCM = [/[A-Z0-9]/i, /[A-Z0-9]/i, /[A-Z0-9]/i, '-', /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, '-', /[A-Z0-9]/i, /[A-Z0-9]/i, /[A-Z0-9]/i,]
 
   //las propiedades del cliente
   cliente: Cliente = new Cliente();
-  compras: any[] = []; 
+  compras: any[] = [];
   documentos_cliente: any[] = [];
   documentos_conyuge: any[] = [];
 
@@ -51,9 +51,10 @@ export class EditarClienteComponent implements OnInit {
   estados: any[] = [];
 
   //selector de compras
-  selection= new SelectionModel<any>(false);
+  selection = new SelectionModel<any>(false);
 
-
+  //la tab seleccionada inicialmente
+  tab_selected: string="Cliente";
 
   selectedOption: string;
   compra_selected: any = {};
@@ -109,35 +110,63 @@ export class EditarClienteComponent implements OnInit {
     console.log("mensaje alerta", this.form.value)
   }
 
-  selectCompra_old(compra, event) {
 
-
-    compra.selected = !compra.selected;
-    if (compra.selected) {
-      this.compra_selected = compra;
-    } else {
-      this.compra_selected = {};
-    };
-  }
-
-  
   selectCompra(compra) {
+    
+    //this.selection.isEmpty();
+    //this.selection.hasValue();
 
     this.selection.toggle(compra);
-    if (this.selection.selected.length>0) {
-      this.compra_selected= this.selection.selected[0];
-      
+    if (this.selection.selected.length > 0) {
+      this.compra_selected = this.selection.selected[0];
+
     } else {
-      this.compra_selected={};
+      this.compra_selected = {};
     }
 
   }
 
-  
 
-  showSelection(){
+
+  showSelection() {
     console.log(this.selection.selected);
-    
+
+  }
+
+  guardarGenerales() {
+    console.log("guardar datos generales", this.tab_selected);
+
+  }
+
+  guardarLaborales() {
+    console.log("guardar datos laborales", this.tab_selected);
+
+  }
+
+  guardarApoderado() {
+    console.log("guardar datos apoderado", this.tab_selected);
+
+  }
+
+  guardarConyuge() {
+    console.log("guardar datos conyuge", this.tab_selected);
+
+  }
+
+  guardarInmueble() {
+    console.log("guardar datos inmueble", this.tab_selected);
+
+  }
+
+  guardarAlerta() {
+    console.log("guardar datos alerta", this.tab_selected);
+
+  }
+
+  onTabChange(event: MatTabChangeEvent) {
+    //console.log("tabChange", event.tab.textLabel, event.tab);
+    this.tab_selected= event.tab.textLabel;
+
   }
 
 
@@ -300,4 +329,31 @@ export class EditarClienteComponent implements OnInit {
   onFechaChange() {
 
   }
+
+  totalPagosRealizados() {
+    let total = 0;
+
+    if (this.compra_selected.pagos) {
+      this.compra_selected.pagos.forEach(pago => {
+        total += +pago.monto;
+
+      });
+    }
+    return total;
+  }
+
+  saldoPendiente() {
+
+    let pendiente = 0;
+
+    if (this.compra_selected.valor_operacion) {
+
+      pendiente = +this.compra_selected.valor_operacion - this.totalPagosRealizados();
+
+    }
+    return pendiente;
+
+  }
+
+
 }
