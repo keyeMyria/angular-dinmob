e96c7b74f667695a3336d7a156385fcc18fb026b
Ramiro Arechar <ramiro@arechar>
2018-01-29 14:03:48 -0600

    alertas clientes

--- src/app/components/ventas/alerta-clientes/alerta-clientes.component.ts
+++ src/app/components/ventas/alerta-clientes/alerta-clientes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import { ClientesService } from 'app/services/clientes.service';
+import { of } from "rxjs/observable/of";
 
 
 @Component({
@@ -8,28 +10,56 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./alerta-clientes.component.scss']
 })
 export class AlertaClientesComponent implements OnInit {
-  clientes: any;
-  cliente: any;
+  clientes: any[] = [];
   obras: any = [];
-  obra: any = {};
+
   obra_selected: string = "";
 
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
+    private clienteSrv: ClientesService
   ) { }
 
   ngOnInit() {
+
     this.route.data
       .subscribe((data: { obras: any[] }) => {
-        //console.log("resusltado resolve ", data);
+        //console.log("resultado resolve ", data);
         this.obras = data.obras;
       });
+
+      this.route.paramMap
+      .switchMap((params: ParamMap) => {
+        if (params.has("obra")) {
+          this.obra_selected = params.get("obra");
+          return this.clienteSrv.getAlertasObra(params.get("obra"));
+        } else {
+          return of([]);
+        }
+
+      }).subscribe(clientes => {
+        this.clientes = clientes;
+      });
+
+
   }
 
   editarCliente(cliente) {
     this.router.navigate(["/editar-cliente", cliente.id_cliente]);
+  }
+
+  
+  cargarObra(id_obra) {
+
+    if (id_obra) {
+      this.router.navigate([".", { obra: id_obra }]);
+    } else {
+      this.router.navigate([".", {}]);
+
+    }
+
   }
 
 
