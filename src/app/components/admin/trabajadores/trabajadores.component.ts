e8b8813e4b179c67db493e03b4749e7cc9bf8f61
Ramiro Arechar <arechar@local.com>
2018-01-31 17:53:23 -0600

    admin/trabajadores

--- src/app/components/admin/trabajadores/trabajadores.component.ts
+++ src/app/components/admin/trabajadores/trabajadores.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-trabajadores',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trabajadores.component.scss']
 })
 export class TrabajadoresComponent implements OnInit {
+  loading: boolean;
+  obras: any = [];
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,) { }
 
   ngOnInit() {
+    this.route.data
+    .subscribe((data: { obras: any[] }) => {
+      //console.log("resultado resolve ", data);
+      this.obras = data.obras;
+    });
+    //this.loading = true;
   }
 
 }
