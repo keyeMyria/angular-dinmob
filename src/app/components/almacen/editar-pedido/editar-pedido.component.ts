d98a8c75c54553a97191844504721b7300f051ea
Ramiro Arechar <ramiro@arechar>
2018-04-19 15:24:52 -0500

    editar pedido

--- src/app/components/almacen/editar-pedido/editar-pedido.component.ts
+++ src/app/components/almacen/editar-pedido/editar-pedido.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import { MatSnackBar } from '@angular/material';
+import { Observable } from 'rxjs/Observable';
+import { ObrasService } from 'app/services/obras.service';
+import { PedidoService } from '../../../services/pedido.service';
 
 @Component({
   selector: 'app-editar-pedido',
@@ -7,9 +13,297 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarPedidoComponent implements OnInit {
 
-  constructor() { }
+  mobileQuery: MediaQueryList;
+  private _mobileQueryListener: () => void;
+
+  obras: any = [];
+  obra_selected: string = "";
+  obra: any;
+  // partidas: any = [];
+  lote_selected: any = {};
+  lotes_pedido: any = [];
+  lotePedido_selected: any = "";
+  insumos: any = [];
+  insumos_pedido: any = [];
+  pedido: any = {};
+
+  usuario: any;
+
+  constructor(
+    private media: MediaMatcher,
+    private changeDetectorRef: ChangeDetectorRef,
+    private route: ActivatedRoute,
+    private router: Router,
+    public snackBar: MatSnackBar,
+    private obraSrv: ObrasService,
+    private pedidoSrv: PedidoService
+  ) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
 
   ngOnInit() {
+
+    this.route.data
+      .subscribe((data: { obras: any[], usuario: any }) => {
+        this.obras = data.obras;
+        this.usuario = data.usuario;
+      });
+
+    this.route.paramMap
+      .switchMap((params: ParamMap) => {
+        if (params.has("obra")) {
+          this.obra_selected = params.get("obra");
+          return this.obraSrv.getAcordeonManzanas(params.get("obra"));
+        } else {
+          return Observable.of({ datos: {} });
+        }
+      }).subscribe(obra => {
+        console.log("obra", obra);
+        this.obra = obra;
+
+        this.lotes_pedido = [];
+        this.lotePedido_selected = "";
+      });
+
+      this.pedidoSrv.getPedidoEditable(12)
+        .subscribe((res: any) => {
+          console.log(res);
+          
+        }
+
   }
+
+  cargarObra(id_obra) {
+
+    if (id_obra) {
+      this.router.navigate([".", { obra: id_obra }]);
+    } else {
+      this.router.navigate([".", {}]);
+
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener)
+
+  }
+
+  loteEstaEnPedido(id_lote) {
+    let encontrado = this.lotes_pedido.find(lote => lote.id_lote == id_lote);
+    if (encontrado === undefined) {
+      //console.log("encontrado undefined");
+      return false;
+    } else {
+      //console.log("encontrado", encontrado);
+      return true;
+    }
+
+  };
+
+  getLote(lote) {
+
+    if (!this.loteEstaEnPedido(lote.id_lote)) {
+      this.pedidoSrv.getLote(lote.id_lote)
+        .subscribe((response: any) => {
+          lote.partidas = response.partidas;
+          lote.insumos_extra = response.insumos_extra;
+          this.lotes_pedido.push(lote);
+          this.lotePedido_selected = this.lotes_pedido[this.lotes_pedido.length - 1];
+
+        }, (error) => {
+
+        });
+    }
+
+  }
+
+  countInsumosPedido(insumos) {
+    let count = 0;
+
+    for (let i = 0; i < insumos.length; i++) {
+      if (insumos[i].requerido > 0) {
+        count++;
+      }
+    }
+    return count;
+
+  }
+
+  countInsumosPedidoPartida(partida) {
+    let count = 0;
+
+    for (let i = 0; i < partida.subpartidas.length; i++) {
+      count += this.countInsumosPedido(partida.subpartidas[i].insumos);
+    }
+    return count;
+
+  }
+
+  delLotePedido() {
+    let i = this.lotes_pedido.indexOf(this.lotePedido_selected);
+    this.lotes_pedido.splice(i, 1);
+    this.lotePedido_selected = "";
+  }
+
+  arrayUnique(origen, array) {
+
+    for (var i = 0; i < array.length; i++) {
+
+      for (var j = 0; j < origen.length; j++) {
+
+        // si el insumo ya está entonces sumamos las cantidades
+        if (origen[j].id_insumo === array[i].id_insumo) {
+          origen[j].requerido = Number(origen[j].requerido) + Number(array[i].requerido);
+          break;
+        }
+
+      }
+
+      // si no encontramos el insumo entonces lo añadimos
+      if (j === origen.length) {
+        //console.log("nuevo insumo", array[i]);
+        let newInsumo = {
+          id_insumo_partida: array[i].id_insumo_partida,
+          id_insumo: array[i].id_insumo,
+          extra: array[i].extra,
+          insumo: array[i].insumo,
+          unidad: array[i].unidad,
+          requerido: array[i].requerido
+        };
+        origen.push(newInsumo);
+      }
+
+    }
+
+
+  }
+
+  getInsumosAcumulados() {
+
+    this.insumos = [];
+
+    //buscamos los insumos de las partidas
+    for (let i = 0; i < this.lotes_pedido.length; i++) {
+
+      //insumos de las partidas
+      for (let j = 0; j < this.lotes_pedido[i].partidas.length; j++) {
+
+        let insumosPartida = this.lotes_pedido[i].partidas[j].insumos.filter(insumo => insumo.requerido > 0);
+        this.arrayUnique(this.insumos, insumosPartida);
+
+        //insumos de las subpartidas
+        for (let k = 0; k < this.lotes_pedido[i].partidas[j].subpartidas.length; k++) {
+
+          let insumosSubpartida = this.lotes_pedido[i].partidas[j].subpartidas[k].insumos.filter(insumo => insumo.requerido > 0);
+          this.arrayUnique(this.insumos, insumosSubpartida);
+
+        }
+
+      }
+
+      //buscamos los insumos extras  
+      let insumosExtra = this.lotes_pedido[i].insumos_extra.filter(insumo => insumo.requerido > 0);
+      this.arrayUnique(this.insumos, insumosExtra);
+
+    }
+
+  }
+
+  getInsumosSinAcumular() {
+
+    this.insumos_pedido = [];
+
+
+    //buscamos los insumos de las partidas
+    for (var i = 0; i < this.lotes_pedido.length; i++) {
+
+      //insumos de las partidas
+      for (var j = 0; j < this.lotes_pedido[i].partidas.length; j++) {
+
+        //console.log("insumos partida");
+        this.concatInsumosPedido(this.insumos_pedido, this.lotes_pedido[i].partidas[j].insumos, this.lotes_pedido[i].id_lote);
+
+        //insumos de las subpartidas
+        for (var k = 0; k < this.lotes_pedido[i].partidas[j].subpartidas.length; k++) {
+          //console.log("insumos subpartida");
+          this.concatInsumosPedido(this.insumos_pedido, this.lotes_pedido[i].partidas[j].subpartidas[k].insumos, this.lotes_pedido[i].id_lote);
+
+        }
+      }
+
+      //buscamos los insumos extras
+      //console.log("insumos extra");
+      this.concatInsumosPedido(this.insumos_pedido, this.lotes_pedido[i].insumos_extra, this.lotes_pedido[i].id_lote);
+    }
+
+  }
+
+
+  concatInsumosPedido(pedido, insumos, id_lote) {
+    //console.log("concatInsumosPedido");
+    for (var i = 0; i < insumos.length; i++) {
+
+
+      if (insumos[i].requerido > 0) {
+
+        /*    var id_insumo_partida = insumos[i].id_insumo_partida;
+           if (insumos[i].extra === "1") {
+             id_insumo_partida = null;
+           } */
+
+
+        let newInsumo = {
+          id_lote: id_lote,
+          id_insumo_partida: insumos[i].extra === "1" ? null : insumos[i].id_insumo_partida,
+          id_insumo: insumos[i].id_insumo,
+          unidad: insumos[i].unidad,
+          cantidad: insumos[i].requerido,
+          extra: insumos[i].extra
+        };
+        pedido.push(newInsumo);
+
+      }
+
+    }
+
+  }
+
+
+
+
+  addPedido() {
+
+    this.pedido.id_obra = this.obra.obra.id_obra;
+
+    this.getInsumosSinAcumular();
+    console.log("insumos", this.insumos_pedido);
+
+    // si los el array de insumos está vacío mostrar un aviso
+
+    console.log("pedido", this.pedido);
+
+    this.pedido.id_usuario = this.usuario.id_usuario;
+    this.pedidoSrv.createPedido(this.pedido, this.insumos_pedido)
+      .subscribe(respuesta => {
+
+        console.log("respuesta", respuesta);
+
+
+        this.lotes_pedido = [];
+        this.lotePedido_selected = "";
+        this.insumos = [];
+
+      }, (error) => {
+
+        this.lotes_pedido = [];
+        this.lotePedido_selected = "";
+        this.insumos = [];
+      });
+
+
+  }
+
 
 }
