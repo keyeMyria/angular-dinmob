7472406943d9480f123b16776ba964a5b4487e3e
Ramiro Arechar <ramiro@arechar>
2018-05-24 16:28:21 -0500

    creamos la constante roles

---
+++ src/app/constantes/roles.ts
@@ -0,0 +1,14 @@
+export enum Rol {
+
+    Administrador = 1,
+    Control = 2,
+    Residente = 3,
+    Almacenista = 4,
+    Contabilidad = 5,
+    Ventas = 6,
+    AsesorVentas = 7,
+    Creditos = 8,
+    Recepcion = 9,
+    ControlAlmacen = 10
+
+}
