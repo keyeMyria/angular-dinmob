2ddbfb0924997fcb13d6477131fcb7336b95144f
Ramiro Arechar <ramiro@arechar>
2018-06-20 14:52:53 -0500

    scroll to top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+//import { filter } from 'rxjs/operators';
+import 'rxjs/add/operator/filter';
 
 
 @Component({
@@ -10,6 +14,27 @@ import { Component } from '@angular/core';
 
 
 export class AppComponent {
-  
+  routerSubscription: Subscription;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.routerSubscription = this.router.events
+      .filter(event => event instanceof NavigationEnd)
+      .subscribe(event => {
+        //console.log("scrolltotop");
+
+        /*    if (!(event instanceof NavigationEnd)) {
+             return;
+           } */
+        //window.scrollTo(0, 0);
+        document.querySelector('.mat-sidenav-content').scrollTop = 0;
+      });
+  }
+
+  ngOnDestroy() {
+    this.routerSubscription.unsubscribe();
+  }
+
 }
 
