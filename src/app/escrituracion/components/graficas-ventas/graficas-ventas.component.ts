f9c0a2a56886de6587e036f6d3744d0344a2bcb2
Ramiro Arechar <ramiro@arechar>
2018-07-21 13:04:21 -0500

    cambio de icono

--- src/app/escrituracion/components/graficas-ventas/graficas-ventas.component.ts
+++ src/app/escrituracion/components/graficas-ventas/graficas-ventas.component.ts
@@ -7,34 +7,180 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraficasVentasComponent implements OnInit {
 
-  single = [
+  data = [
     {
-      "name": "Germany",
-      "value": 8940000
+      "name": "Enero",
+      "value": 5
     },
     {
-      "name": "USA",
-      "value": 5000000
+      "name": "Febrero",
+      "value": 7
     },
     {
-      "name": "France",
-      "value": 7200000
+      "name": "Marzo",
+      "value": 4
+    },
+    {
+      "name": "Abril",
+      "value": 4
+    },
+    {
+      "name": "Mayo",
+      "value": 4
+    },
+    {
+      "name": "Junio",
+      "value": 5
+    },
+    {
+      "name": "Julio",
+      "value": 4
+    },
+    {
+      "name": "Agosto",
+      "value": 6
+    },
+    {
+      "name": "Septiembre",
+      "value": 3
+    },
+    {
+      "name": "Octubre",
+      "value": 1
+    },
+    {
+      "name": "Noviembre",
+      "value": 5
+    },
+    {
+      "name": "Diciembre",
+      "value": 4
     }
   ];
 
-  multi: any[];
+  data2 = [
+    {
+      "name": "Vendedor",
+      "series": [
+        {
+          "name": "Enero",
+          "value": 5
+        },
+        {
+          "name": "Febrero",
+          "value": 7
+        },
+        {
+          "name": "Marzo",
+          "value": 4
+        },
+        {
+          "name": "Abril",
+          "value": 4
+        },
+        {
+          "name": "Mayo",
+          "value": 4
+        },
+        {
+          "name": "Junio",
+          "value": 5
+        },
+        {
+          "name": "Julio",
+          "value": 4
+        },
+        {
+          "name": "Agosto",
+          "value": 6
+        },
+        {
+          "name": "Septiembre",
+          "value": 3
+        },
+        {
+          "name": "Octubre",
+          "value": 1
+        },
+        {
+          "name": "Noviembre",
+          "value": 5
+        },
+        {
+          "name": "Diciembre",
+          "value": 4
+        }
+      ]
+    },
+    {
+      "name": "Equipo",
+      "series": [
+        {
+          "name": "Enero",
+          "value": 7
+        },
+        {
+          "name": "Febrero",
+          "value": 9
+        },
+        {
+          "name": "Marzo",
+          "value": 6
+        },
+        {
+          "name": "Abril",
+          "value": 6
+        },
+        {
+          "name": "Mayo",
+          "value": 7
+        },
+        {
+          "name": "Junio",
+          "value": 7
+        },
+        {
+          "name": "Julio",
+          "value": 6
+        },
+        {
+          "name": "Agosto",
+          "value": 8
+        },
+        {
+          "name": "Septiembre",
+          "value": 5
+        },
+        {
+          "name": "Octubre",
+          "value": 3
+        },
+        {
+          "name": "Noviembre",
+          "value": 7
+        },
+        {
+          "name": "Diciembre",
+          "value": 6
+        }
+      ]
+    }
+  ];
 
-  view: any[] = [700, 400];
+
+
+  view; //: any[] = [700, 400];
 
   // options
   showXAxis = true;
   showYAxis = true;
   gradient = false;
   showLegend = true;
+  showDataLabel = true;
   showXAxisLabel = true;
-  xAxisLabel = 'Country';
+  xAxisLabel = '2018';
   showYAxisLabel = true;
-  yAxisLabel = 'Population';
+  yAxisLabel = 'Ventas';
 
   colorScheme = {
     domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
