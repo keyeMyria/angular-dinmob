1c332a43d8a6cc13ffb914b0b8096084696b9173
Ramiro Arechar <ramiro@arechar>
2018-07-23 14:40:31 -0500

    grafica ventas por vendedor

---
+++ src/app/components/admin/graficas-ventas-vendedor/graficas-ventas-vendedor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graficas-ventas-vendedor',
+  templateUrl: './graficas-ventas-vendedor.component.html',
+  styleUrls: ['./graficas-ventas-vendedor.component.scss']
+})
+export class GraficasVentasVendedorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
