6e557d604814c95793ca69d7eb52674f06c189a9
Acer <acer@arechar.com>
2018-07-25 17:37:24 -0500

    historial ventas

--- src/app/escrituracion/components/lotes-en-tramite/lotes-en-tramite.component.ts
+++ src/app/escrituracion/components/lotes-en-tramite/lotes-en-tramite.component.ts
@@ -6,6 +6,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lotes-en-tramite.component.scss']
 })
 export class LotesEnTramiteComponent implements OnInit {
+  lotes = [
+    {
+      id_cliente: "1",
+      manzana: "Mazana 1",
+      lote: "Lote 2",
+      precio: "700000",
+      id_estado: "1",
+      fecha_estado: "2018-06-21",
+      estado_venta: "Libre"
+    },
+    {
+      id_cliente: "2",
+      manzana: "Mazana 2",
+      lote: "Lote 1",
+      precio: "700000",
+      id_estado: "1",
+      fecha_estado: "2018-03-10",
+      estado_venta: "Libre"
+    },
+    {
+      id_cliente: "3",
+      manzana: "Mazana 3",
+      lote: "Lote 4",
+      precio: "700000",
+      id_estado: "0",
+      fecha_estado: "2018-04-20",
+      estado_venta: "Contrato"
+    },
+    {
+      id_cliente: "4",
+      manzana: "Mazana 4",
+      lote: "Lote 5",
+      precio: "700000",
+      id_estado: "1",
+      fecha_estado: "2018-03-01",
+      estado_venta: "Libre"
+    },
+    {
+      id_cliente: "5",
+      manzana: "Mazana 5",
+      lote: "Lote 6",
+      precio: "700000",
+      id_estado: "0",
+      fecha_estado: "2018-06-21",
+      estado_venta: "Contrato"
+    }
+  ]
 
   constructor() { }
 
