476f948703f0bfbe9108277a0ffca1163c4deee4
Acer <acer@arechar.com>
2018-07-27 17:26:20 -0500

    CONFIGURACIÓN

--- src/app/escrituracion/components/configuracion-escrituracion/configuracion-escrituracion.component.ts
+++ src/app/escrituracion/components/configuracion-escrituracion/configuracion-escrituracion.component.ts
@@ -10,175 +10,28 @@ import { EstadoService } from '../../services/estado.service';
 })
 export class ConfiguracionEscrituracionComponent implements OnInit {
   form: FormGroup;
-
-  estados_infonavit = [
-    {
-      nombre: "Apartado",
-      id_estado_infonavit: "1",
-
-    },
-    {
-      nombre: "Expediente",
-      id_estado_infonavit: "2",
-
-    },
-    {
-      nombre: "Infonavit",
-      id_estado_infonavit: "3",
-
-    },
-    {
-      nombre: "Firma",
-      id_estado_infonavit: "4",
-
-    },
-    {
-      nombre: "Entrega",
-      id_estado_infonavit: "5",
-
-    }
-  ];
-  estados_fovissste = [
-    {
-      nombre: "Apartado",
-      id_estado_fovissste: "1",
-
-    },
-    {
-      nombre: "Expediente",
-      id_estado_fovissste: "2",
-
-    },
-    {
-      nombre: "Reactivación",
-      id_estado_fovissste: "3",
-
-    },
-    {
-      nombre: "Avaluo",
-      id_estado_fovissste: "4",
-
-    },
-    {
-      nombre: "Validación CUV",
-      id_estado_fovissste: "5",
-
-    },
-    {
-      nombre: "SOFOM",
-      id_estado_fovissste: "6",
-
-    },
-    {
-      nombre: "Instrucción Notarial",
-      id_estado_fovissste: "57",
-
-    },
-    {
-      nombre: "Escrituración",
-      id_estado_fovissste: "8",
-
-    },
-    {
-      nombre: "Entrega",
-      id_estado_fovissste: "9",
-
-    },
-  ];
-  estados_contado = [
-    {
-      nombre: "Apartado",
-      id_estado_contado: "1"
-    },
-    {
-      nombre: "Expediente",
-      id_estado_contado: "2"
-    },
-    {
-      nombre: "Notaria",
-      id_estado_contado: "3"
-    },
-    {
-      nombre: "Firma",
-      id_estado_contado: "4"
-    },
-    {
-      nombre: "Entrega",
-      id_estado_contado: "5"
-    }
-  ];
-  estados_bancos = [
-    {
-      nombre: "Apartado",
-      id_estado_banco: "1"
-    },
-    {
-      nombre: "Expediente",
-      id_estado_banco: "2"
-    },
-    {
-      nombre: "Autorización Crédito",
-      id_estado_banco: "3"
-    },
-    {
-      nombre: "Firma",
-      id_estado_banco: "4"
-    },
-    {
-      nombre: "Entrega",
-      id_estado_banco: "5"
-    }
-  ];
-  estados_cofinanciamiento = [
-    {
-      nombre: "Apartado",
-      id_estado_confinanciamiento: "1"
-    },
-    {
-      nombre: "Expediente",
-      id_estado_confinanciamiento: "2"
-    },
-    {
-      nombre: "Autorización Crédito",
-      id_estado_confinanciamiento: "3"
-    },
-    {
-      nombre: "Infonavit",
-      id_estado_confinanciamiento: "4"
-    },
-    {
-      nombre: "Entrega",
-      id_estado_confinanciamiento: "5"
-    }
-  ];
-  tipos_creditos = [
-    {
-      credito: "Infonavit",
-      id_tipo_credito: "1"
-    },
-    {
-      credito: "Fovissste",
-      id_tipo_credito: "2"
-    },
-    {
-      credito: "Contado",
-      id_tipo_credito: "3"
-    },
-    {
-      credito: "Banco",
-      id_tipo_credito: "4"
-    },
-    {
-      credito: "Cofinanciamiento",
-      id_tipo_credito: "5"
-    }
-  ]
-
   estados: any = [];
+  formInfonavit: FormGroup;
+  formContado: FormGroup;
+  formFovissste: FormGroup;
+  formBancos: FormGroup;
+  formConfinanciamiento: FormGroup;
+
   constructor(
     private fb: FormBuilder,
     private estadoSrv: EstadoService
   ) {
+    this.formInfonavit = this.fb.group({
+    });
+    this.formContado = this.fb.group({
+    });
+    this.formFovissste = this.fb.group({
+    });
+    this.formBancos = this.fb.group({
+    });
+    this.formConfinanciamiento = this.fb.group({
+
+    });
   }
 
   ngOnInit() {
@@ -190,8 +43,21 @@ export class ConfiguracionEscrituracionComponent implements OnInit {
       });
   }
 
-  guardar() {
-
+  guardarInfonavit() {
   }
+
+  guardarContado() {
+  }
+
+  guardarFovissste() {
+  }
+
+  guardarBancos() {
+  }
+
+  guardarCofinanciamiento() {
+  }
+
+
 
 }
