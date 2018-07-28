428d052ec5a873357abd9fddbfbdb7795a8a172c
Ramiro Arechar <ramiro@arechar>
2018-07-28 17:13:38 -0500

    card link

--- src/app/components/admin/estadisticas-admin/estadisticas-admin.component.ts
+++ src/app/components/admin/estadisticas-admin/estadisticas-admin.component.ts
@@ -3,6 +3,7 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { ObrasService } from 'app/services/obras.service';
 import { of } from "rxjs";
 import { MatSnackBar } from '@angular/material';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-estadisticas-admin',
@@ -13,26 +14,38 @@ export class EstadisticasAdminComponent implements OnInit {
   obras: any = [];
   estadisticas: any = {};
   obra_selected: string = "";
+  obra_default: any;
+  usuario: any;
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private obraSrv: ObrasService,
-    public snackBar: MatSnackBar,
-  ) { }
+    private snackBar: MatSnackBar,
+    private authSrv: AuthService
+  ) {
+
+    this.usuario = this.authSrv.usuario;
+
+    if (this.usuario.id_obra_default) {
+      this.obra_default = { obra: this.usuario.id_obra_default };
+    } else {
+      this.obra_default = {};
+    }
+  }
 
   ngOnInit() {
 
 
     this.route.data
       //.subscribe((data: { obras: any[], estadisticas: any }) => {
-        .subscribe((data:{ datos:{estadisticas:any, obras:any}})=>{
-          //console.log("data", data.datos);
-          
+      .subscribe((data: { datos: { estadisticas: any, obras: any } }) => {
+        //console.log("data", data.datos);
+
         //this.obras = data.obras;
         //this.estadisticas = data.estadisticas;
-        this.obras= data.datos.obras;
-        this.estadisticas= data.datos.estadisticas;
+        this.obras = data.datos.obras;
+        this.estadisticas = data.datos.estadisticas;
         this.obra_selected = this.route.snapshot.params["obra"] ? this.route.snapshot.params["obra"] : "";
       });
 
